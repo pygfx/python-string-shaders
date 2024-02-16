b8bfb7e912406da6d6162775bdfa972d73f2a31b
Almar Klein <almar@almarklein.org>
2024-02-16 09:45:51 +0100

    Fix that shader syntax persisted past the end of the Python string

---
+++ example.py
@@ -0,0 +1,26 @@
+# A comment
+for i in range(10):
+    pass
+
+
+shader1 = """//wgsl
+fn foo(f32) {
+    return vec2<f32>(1.0m 1.0);
+}
+"""
+
+
+# A comment
+for i in range(10):
+    pass
+
+
+shader2 = """//wgsl
+fn foo(f32) {
+    return vec2<f32>(1.0m 1.0);
+}
+"""
+
+# A comment
+for i in range(10):
+    pass
